3e5dc86cd3723b572c63cb75830202b80ccd41e2
Danval-003 <danarvare2003@gmail.com>
2024-05-15 23:15:10 -0600

    Add username verification into server.cpp

--- server.cpp
+++ server.cpp
@@ -9,18 +9,31 @@
 #include "constants.h"
 #include "./src/sendFunction.h"
 #include "nlohmann/json.hpp"
+#include <mutex>
+#include <queue>
 
 using json = nlohmann::json;
+
+// Estructura para almacenar información de los clientes
+json clients;
+// Estructura para prevenir el acceso simultáneo a la estructura de clientes
+std::mutex clientsMutex;
+// Bandera para indicar al hilo de clientes que hay nuevos clientes
+bool newClients = false;
+
 
 struct ClientInfo {
     int socket;            // Socket asociado con el cliente
     std::string ipAddress; // Dirección IP del cliente en formato legible
+    std::queue<chat::Response>* responses; // Cola de respuestas para el cliente
+    std::mutex responsesMutex; // Mutex para prevenir el acceso simultáneo a la cola de respuestas
+    bool* connected; // Indica si el cliente está conectado
 };
 
-void* handleClient(void* arg) {
+void* handleListenClient(void* arg) {
     ClientInfo* info = (ClientInfo*)arg;
     int clientSocket = info->socket;
-    printf("Cliente conectadon");
+
     // Recive the request
     chat::Request first_request;
     // First request are the user name
@@ -46,8 +59,33 @@ void* handleClient(void* arg) {
     std::cout << "Usuario: " << userName << std::endl;
 
     first_request.Clear();
+    json client;
+    client["ip"] = info->ipAddress;
+    client["socket"] = clientSocket;
+    client["status"] = "online";
 
-    while (1) {
+    // Add the user to the clients list, but first verify if the user is already in the list
+    clientsMutex.lock();
+    if (clients.find(userName) != clients.end()) {
+        clientsMutex.unlock();
+        std::cerr << "El usuario ya está registrado." << std::endl;
+        chat::Response badResponse;
+        badResponse.set_operation(chat::REGISTER_USER);
+        badResponse.set_status_code(chat::BAD_REQUEST);
+        badResponse.set_message("El usuario ya está registrado.");
+        info->responsesMutex.lock();
+        info->responses->push(badResponse);
+        info->responsesMutex.unlock();
+        info->connected = new bool(false);
+        return NULL;
+    } else{
+        clients[userName] = client;
+        clientsMutex.unlock();
+    }
+    
+    
+
+    while (info->connected) {
         chat::Request request;
         int status = getRequest(&request, clientSocket);
         if (status == -1) {
@@ -74,6 +112,33 @@ void* handleClient(void* arg) {
 
     return NULL; // Agregar declaración de retorno
 }
+
+
+void* handleResponseClient(void* arg){
+    ClientInfo* info = (ClientInfo*)arg;
+    int clientSocket = info->socket;
+
+    while (info->connected || !info->responses->empty()) {
+        chat::Response response;
+        info->responsesMutex.lock();
+        if (!info->responses->empty()) {
+            response = info->responses->front();
+            info->responses->pop();
+        }
+        info->responsesMutex.unlock();
+        int status = sendResponse(&response, clientSocket);
+        if (status == -1) {
+            std::cerr << "Error al enviar la respuesta." << std::endl;
+            break;
+        }
+    }
+
+    // Cerrar el socket del cliente
+    close(clientSocket);
+
+    return NULL; // Agregar declaración de retorno
+}
+
 
 int main() {
     // Crear el socket del servidor
@@ -122,14 +187,24 @@ int main() {
         ClientInfo clientInfo;
         clientInfo.socket = clientSocket;
         clientInfo.ipAddress = clientIP;
+        clientInfo.responses = new std::queue<chat::Response>();
+        clientInfo.connected = new bool(true);
 
         std::cout << "Cliente conectado. Desde "<<clientInfo.ipAddress<<":" <<clientInfo.socket<<"."<< std::endl;
         std::cout << BUFFER_SIZE << std::endl;
 
 
+
         // Crear hilo para manejar el cliente
         pthread_t clientThread;
-        if (pthread_create(&clientThread, NULL, handleClient, (void*)&clientInfo) != 0) {
+        if (pthread_create(&clientThread, NULL, handleListenClient, (void*)&clientInfo) != 0) {
+            std::cerr << "Error al crear el hilo para el cliente." << std::endl;
+            close(clientSocket);
+            continue; // Continuar aceptando nuevas conexiones
+        }
+
+        pthread_t responseThread;
+        if (pthread_create(&responseThread, NULL, handleResponseClient, (void*)&clientInfo) != 0) {
             std::cerr << "Error al crear el hilo para el cliente." << std::endl;
             close(clientSocket);
             continue; // Continuar aceptando nuevas conexiones
